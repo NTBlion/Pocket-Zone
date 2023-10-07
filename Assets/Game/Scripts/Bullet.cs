780ea7e7db4ed0c3c59e7a19cde21e39cbcd829d
ntblion <ntb.video@gmail.com>
2023-10-07 18:23:37 +0300

    Add Bullet

--- Assets/Game/Scripts/Bullet.cs
+++ Assets/Game/Scripts/Bullet.cs
@@ -1,12 +1,23 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rigidbody;
-    
+    [SerializeField] private float _delayBeforeDestroy;
+
+    private WaitForSeconds _delay;
     public void Init(Vector2 velocity)
     {
         _rigidbody.velocity = velocity;
+        _delay = new WaitForSeconds(_delayBeforeDestroy);
+        StartCoroutine(DestroyBullet());
+    }
+
+    private IEnumerator DestroyBullet()
+    {
+        yield return _delay;
+        Destroy(gameObject);
     }
 }
