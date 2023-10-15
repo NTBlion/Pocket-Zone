954771d326d7e45da5b879a3da3e7a9a382d4c46
NTBlion <103928473+NTBlion@users.noreply.github.com>
2023-10-15 23:47:08 +0300

    Init

--- Assets/_Game/Scripts/Spawners/ItemSpawner.cs
+++ Assets/_Game/Scripts/Spawners/ItemSpawner.cs
@@ -4,10 +4,10 @@ using Random = UnityEngine.Random;
 
 public class ItemSpawner : MonoBehaviour
 {
-    [SerializeField] private Item[] _item;
+    //[SerializeField] private Item[] _item;
 
     public void Spawn()
     {
-        Instantiate(_item[Random.Range(0,_item.Length)],transform.position,quaternion.identity);
+     //   Instantiate(_item[Random.Range(0,_item.Length)],transform.position,quaternion.identity);
     }
 }
